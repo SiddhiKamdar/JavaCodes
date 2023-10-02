e898f5f373f7fff8e6b0b977ed4acf5d67231831
SiddhiKamdar <127606128+SiddhiKamdar@users.noreply.github.com>
2023-10-02 12:42:00 +0530

    Add files via upload

---
+++ SelectionSort.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class SelectionSort{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        int len = arr.length;
+        for (int i = 0; i < len - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < len; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+        System.out.println("Sorted array :");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
